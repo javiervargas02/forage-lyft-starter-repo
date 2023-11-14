fff12515bfaec5d39a9ce7a26f4dcdb05dda6817
javiervargas02 <javier.v.021204@gmail.com>
2023-11-13 23:39:57 -0600

    Refactored Code javivargas02

--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -1,13 +1,11 @@
-from abc import ABC
+from .engine import engine
 
-from car import Car
-
-
-class WilloughbyEngine(Car, ABC):
-    def __init__(self, last_service_date, current_mileage, last_service_mileage):
-        super().__init__(last_service_date)
-        self.current_mileage = current_mileage
+class willoughby(engine):
+    def __init__(self, last_service_mileage : int, current_mileage : int):
         self.last_service_mileage = last_service_mileage
+        self.current_mileage = current_mileage
 
-    def engine_should_be_serviced(self):
-        return self.current_mileage - self.last_service_mileage > 60000
+    def needs_service(self) -> bool:
+        service_mileage = self.last_service_mileage
+        current_mileage = self.current_mileage
+        return current_mileage - service_mileage >= 60000
